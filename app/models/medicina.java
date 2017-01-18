f8a271755fce5dbff3ccbbd6635361c04a0b0eb7
Yamilet Hernandez <yamilet_207@hotmail.com>
2017-01-17 18:26:56 -0600

    1 Creación del proyecto y preparación 2 Conexion a una base de datos local en mi computadora 3 Conexion a una base residente en memoria 4 Creación de páginas 5 Insertar y visualizar datos

---
+++ app/models/medicina.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import com.avaje.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+@Entity
+public class medicina extends Model {
+
+    @Id
+    
+    private int id;
+
+    @Constraints.Required
+    private String nombre;
+
+    private String descripcion;
+
+    private int gramos;
+
+    private int precio;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getGramos() {
+        return gramos;
+    }
+
+    public void setGramos(int gramos) {
+        this.gramos = gramos;
+    }
+
+    /**
+     * @return the precio
+     */
+    public int getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+}
