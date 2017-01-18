2748e64c209fe5812d6e605e9c43bbfd4745a6a4
Yamilet Hernandez <yamilet_207@hotmail.com>
2017-01-17 18:42:20 -0600

    1 Creación del proyecto y preparación 2 Conexion a una base de datos local en mi computadora 3 Conexion a una base residente en memoria 4 Creación de páginas 5 Insertar y visualizar datos

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,23 +1,58 @@
 package controllers;
 
 import play.mvc.*;
-
+import play.data.*;
+import models.medicina;
 import views.html.*;
+import javax.inject.Inject;
+import com.avaje.ebean.Model;
+import java.util.ArrayList;
+import java.util.List;
+import static play.libs.Json.toJson;
 
 /**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
+ * This controller contains an action to handle HTTP requests to the
+ * application's home page.
  */
 public class HomeController extends Controller {
 
+    @Inject
+    private FormFactory formFactory;
+
     /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
+     * An action that renders an HTML page with a welcome message. The
+     * configuration in the <code>routes</code> file means that this method will
+     * be called when the application receives a <code>GET</code> request with a
+     * path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("Bienvenido a su farmacia"));
+        return ok(index.render(""));
     }
 
+    public Result agregar() {
+        return ok(agrega.render(""));
+    }
+
+    public Result modificar() {
+        return ok(modifica.render(""));
+    }
+public Result borrar() {
+        return ok(borra.render(""));
+    }
+
+  
+    public Result addMedicina() {
+        Form<medicina> medicinaForm = formFactory.form(medicina.class);
+        medicina medicamento = medicinaForm.bindFromRequest().get();
+        medicamento.save();
+        return redirect(routes.HomeController.agregar());
+    }
+
+   
+    public Result visualizar() {
+        Model.Finder<Integer, medicina> finder = new Model.Finder<>(medicina.class);
+        List<medicina> medicamentos = finder.all();
+        return ok(visualiza.render(medicamentos));
+
+    }
 }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,21 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table medicina (
+  id                            integer not null,
+  nombre                        varchar(255),
+  descripcion                   varchar(255),
+  gramos                        integer,
+  precio                        integer,
+  constraint pk_medicina primary key (id)
+);
+create sequence medicina_seq;
+
+
+# --- !Downs
+
+drop table if exists medicina;
+drop sequence if exists medicina_seq;
+
